0b33e31fbf7ed9c3d4edf3e0d1770c52010dd177
folkengine <gaoler@electronicpanopticon.com>
2022-04-06 11:32:15 -0700

    fix me

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,18 @@
+#![warn(clippy::pedantic)]
 pub fn boop() -> String {
-    "Boop!".to_string()
+  "Boop!".to_string()
 }
 
 pub fn greeting(name: String) -> String {
-    format!("Hello, {}!", name)
+  format!("Hello, {}!", name)
 }
 
 #[cfg(test)]
 mod tests {
-    use super::*;
+  use super::*;
 
-    #[test]
-    fn greeting_test() {
-        assert_eq!("Hello, World!".to_string(), greeting("World".to_string()));
-    }
+  #[test]
+  fn greeting_test() {
+    assert_eq!("Hello, World!".to_string(), greeting("World".to_string()));
+  }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,31 @@
-use clap::{crate_authors, crate_license, crate_name, crate_version, App, Arg, ArgMatches};
+use clap::{crate_authors, crate_description, crate_name, crate_version, Arg, ArgMatches, Command};
 
 const PKG_DESCRIPTION: &str = env!("CARGO_PKG_DESCRIPTION");
 
 fn get_matches() -> ArgMatches {
-    App::new(crate_name!())
-        .version(crate_version!())
-        .author(crate_authors!())
-        .license(crate_license!())
-        .about(PKG_DESCRIPTION)
-        .arg(
-            Arg::new("name")
-                .short('n')
-                .long("name")
-                .about("Greets someone by name")
-                .takes_value(true)
-                .value_name("NAME")
-                .default_value("World"),
-        )
-        .arg("-b, --boop 'Go boop.'")
-        .get_matches()
+  Command::new(crate_name!())
+    .version(crate_version!())
+    .author(crate_authors!())
+    .about(crate_description!())
+    .arg(
+      Arg::new("name")
+        .short('n')
+        .long("name")
+        .about("Greets someone by name")
+        .takes_value(true)
+        .value_name("NAME")
+        .default_value("World"),
+    )
+    .arg("-b, --boop 'Go boop.'")
+    .get_matches()
 }
 
 fn main() {
-    let matches = get_matches();
+  let matches = get_matches();
 
-    if matches.is_present("boop") {
-        println!("{}", changemelib::boop());
-    } else if let Some(i) = matches.value_of("name") {
-        println!("{}", changemelib::greeting(i.to_string()));
-    }
+  if matches.is_present("boop") {
+    println!("{}", changemelib::boop());
+  } else if let Some(i) = matches.value_of("name") {
+    println!("{}", changemelib::greeting(i.to_string()));
+  }
 }
